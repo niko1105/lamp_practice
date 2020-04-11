5fb88bc8a3aba23eedef6485da3a9f67c418dcd7
小西郁弥 <konikoni0803@gmail.com>
2020-04-11 18:49:14 +0900

    追加テーブルの作成#8

--- lamp_dock/append_schema.sql
+++ lamp_dock/append_schema.sql
@@ -2,15 +2,15 @@ CREATE TABLE `sample`.`orders` (
     `order_id` INT(11) NOT NULL AUTO_INCREMENT ,
     `user_id` INT(11) NOT NULL ,
     `created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
-    `updated` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , 
+    `updated` DATETIME on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , 
     PRIMARY KEY (`order_id`)) ENGINE = InnoDB;
 
 CREATE TABLE `sample`.`order_datails` ( 
     `order_detail_id` INT(11) NOT NULL AUTO_INCREMENT , 
     `order_id` INT(11) NOT NULL , 
-    `itgem_id` INT(11) NOT NULL , 
+    `item_id` INT(11) NOT NULL , 
     `price` INT(11) NOT NULL , 
     `amount` INT(11) NOT NULL ,
     `created` DATETIME NULL DEFAULT CURRENT_TIMESTAMP ,
-    `updated` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , 
+    `updated` DATETIME on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , 
     PRIMARY KEY (`order_detail_id`)) ENGINE = InnoDB;
